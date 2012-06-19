f17a1265e2d13f895caf1e39933aa94a1d2f2c03
angelofdev <poya.yahyaviazad@gmail.com>
2012-06-19 19:58:35 +1000

    Removed unused code.

--- src/me/angelofdev/DoOdy/listeners/DoOdyPlayerListener.java
+++ src/me/angelofdev/DoOdy/listeners/DoOdyPlayerListener.java
@@ -228,29 +228,5 @@ public class DoOdyPlayerListener implements Listener {
 			ois.close();
 			return result;
 		}
-	}
-	
-	
-	public class LocationPack implements Serializable  {
-	    private static final long serialVersionUID = -8100514952085724461L;
-	 
-	    private final String  worldname;
-	    private final double x;
-	    private final double y;
-	    private final double z;
-	 
-	    public LocationPack(Location location) {
-	        this.worldname = location.getWorld().getName();
-	        this.x = location.getX();
-	        this.y = location.getY();
-	        this.z = location.getZ();
-	    }
-	 
-	    public Location unpack() {
-	        Location location = new Location(Bukkit.getWorld(this.worldname), this.x, this.y, this.z);
-	 
-	        return location;
-	    }
-	}
-	
+	}	
 }
