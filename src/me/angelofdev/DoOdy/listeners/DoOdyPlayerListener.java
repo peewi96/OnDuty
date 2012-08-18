667c0116fc2d97b21f28e28b513e4084359ef0be
angelofdev <poya.yahyaviazad@gmail.com>
2012-08-19 07:33:31 +1000

    Forgot what I changed lawl

--- src/me/angelofdev/DoOdy/listeners/DoOdyPlayerListener.java
+++ src/me/angelofdev/DoOdy/listeners/DoOdyPlayerListener.java
@@ -23,10 +23,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+
+import me.angelofdev.DoOdy.Log;
 import me.angelofdev.DoOdy.command.DoOdyCommandExecutor;
 import me.angelofdev.DoOdy.config.Configuration;
 
@@ -34,6 +35,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -49,6 +51,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class DoOdyPlayerListener implements Listener {
@@ -56,10 +59,10 @@ public class DoOdyPlayerListener implements Listener {
 	public DoOdyPlayerListener() {
 	}
 
-	public static HashMap<String, ItemStack[]> inventory = new HashMap<String, ItemStack[]>();
-	public static HashMap<String, GameMode> duty = new HashMap<String, GameMode>();
-	public static HashMap<String, Location> location = new HashMap<String, Location>();
 	public static HashMap<String, Integer> expOrb = new HashMap<String, Integer>();
+	public static HashMap<String, ItemStack[]> armour = new HashMap<String, ItemStack[]>();
+	public static HashMap<String, GameMode> duty = new HashMap<String, GameMode>();
+	public static HashMap<String, ItemStack[]> inventory = new HashMap<String, ItemStack[]>();
 	List<Integer> configDropList = Configuration.config.getIntegerList("Duty Deny Drops.whitelist");
 	List<Integer> configStorageDenied = Configuration.config.getIntegerList("Deny Storage.storage");
 	public static List<Integer> configLbTools = Configuration.config.getIntegerList("Allow.LogBlock.Tools");
@@ -69,18 +72,18 @@ public class DoOdyPlayerListener implements Listener {
 	@EventHandler(ignoreCancelled=true)
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
-		String playername = player.getName();
 		if (player.getGameMode() == GameMode.CREATIVE) {
-			if (player.isOp() || duty.containsKey(playername)){
+			String playerName = player.getName();
+			if (player.isOp() || duty.containsKey(playerName) || player.hasPermission("doody.failsafe.bypass")){
 				return;
 			}
 			player.setGameMode(GameMode.SURVIVAL);
 			player.getInventory().clear();
-			if (DoOdyCommandExecutor.myArr.contains(playername)) {
-				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playername));
+			if (DoOdyCommandExecutor.myArr.contains(playerName)) {
+				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playerName));
 			}
-			if (expOrb.containsKey(playername)) {
-				expOrb.remove(playername);
+			if (expOrb.containsKey(playerName)) {
+				expOrb.remove(playerName);
 			}
 		}
 	}
@@ -88,20 +91,56 @@ public class DoOdyPlayerListener implements Listener {
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
-		String playername = player.getName();
-		if (duty.containsKey(playername)) {
+		String playerName = player.getName();
+		if (duty.containsKey(playerName)) {
 			try {
-				player.getInventory().setContents(inventory.get(playername));
-				inventory.remove(playername);
-				player.teleport(location.get(playername));
-				location.remove(playername);
+				if (inventory.containsKey(playerName)) {
+					player.getInventory().setContents(inventory.get(playerName));
+				} else {
+					player.getInventory().clear();
+					try {
+						Integer size = player.getInventory().getSize();
+						Integer i = 0;
+						for(i=0; i < size; i++) {
+					        ItemStack item = new ItemStack(0, 0);
+					        if(Configuration.inventory.getInt(playerName + "." + i.toString() + ".amount", 0) !=0) {
+					        	Integer amount = Configuration.inventory.getInt(playerName + "." + i.toString() + ".amount", 0);
+					        	Integer durability = Configuration.inventory.getInt(playerName + "." + i.toString() + ".durability", 0);
+					        	Integer type = Configuration.inventory.getInt(playerName + "." + i.toString() + ".type", 0);
+					        	item.setAmount(amount);
+					        	item.setTypeId(type);
+					        	item.setDurability(Short.parseShort(durability.toString()));
+					        	player.getInventory().setItem(i, item);
+							}
+						}
+					} catch(Exception e) {
+					}
+				}
+				Configuration.inventory.set(playerName, null);
+				Configuration.inventory.save();
+				player.getInventory().setArmorContents(armour.get(playerName));
+				armour.remove(playerName);
+				World world = Bukkit.getServer().getWorld(Configuration.location.getString(playerName + ".world"));
+				double x = Configuration.location.getDouble(playerName + ".x");
+				double y = Configuration.location.getDouble(playerName + ".y");
+				double z = Configuration.location.getDouble(playerName + ".z");
+				double pit = Configuration.location.getDouble(playerName + ".pitch");
+				double ya = Configuration.location.getDouble(playerName + ".yaw");
+				float pitch = (float) pit;
+				float yaw = (float) ya;
+												
+				Location local = new Location(world, x, y, z, yaw, pitch);
+				player.teleport(local);
+
+				Configuration.location.set(playerName, null);
+				Configuration.location.save();
 				player.setGameMode(GameMode.SURVIVAL);
-				duty.remove(playername);
-				player.setLevel(DoOdyPlayerListener.expOrb.get(playername));
-				expOrb.remove(playername);
-				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playername));
+				duty.remove(playerName);
+				player.setLevel(DoOdyPlayerListener.expOrb.get(playerName));
+				expOrb.remove(playerName);
+				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playerName));
 			} catch (Exception e) {
-				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playername));
+				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playerName));
 				player.setGameMode(GameMode.SURVIVAL);
 				player.getInventory().clear();
 			}
@@ -111,20 +150,56 @@ public class DoOdyPlayerListener implements Listener {
 	@EventHandler
 	public void onPlayerKick(PlayerKickEvent event) {
 		Player player = event.getPlayer();
-		String playername = player.getName();
-		if (duty.containsKey(playername)) {
+		String playerName = player.getName();
+		if (duty.containsKey(playerName)) {
 			try {
-				player.getInventory().setContents(inventory.get(playername));
-				inventory.remove(playername);
-				player.teleport(location.get(playername));
-				location.remove(playername);
+				if (inventory.containsKey(playerName)) {
+					player.getInventory().setContents(inventory.get(playerName));
+				} else {
+					player.getInventory().clear();
+					try {
+						Integer size = player.getInventory().getSize();
+						Integer i = 0;
+						for(i=0; i < size; i++) {
+					        ItemStack item = new ItemStack(0, 0);
+					        if(Configuration.inventory.getInt(playerName + "." + i.toString() + ".amount", 0) !=0) {
+					        	Integer amount = Configuration.inventory.getInt(playerName + "." + i.toString() + ".amount", 0);
+					        	Integer durability = Configuration.inventory.getInt(playerName + "." + i.toString() + ".durability", 0);
+					        	Integer type = Configuration.inventory.getInt(playerName + "." + i.toString() + ".type", 0);
+					        	item.setAmount(amount);
+					        	item.setTypeId(type);
+					        	item.setDurability(Short.parseShort(durability.toString()));
+					        	player.getInventory().setItem(i, item);
+							}
+						}
+					} catch(Exception e) {
+					}
+				}
+				Configuration.inventory.set(playerName, null);
+				Configuration.inventory.save();
+				player.getInventory().setArmorContents(armour.get(playerName));
+				armour.remove(playerName);
+				World world = Bukkit.getServer().getWorld(Configuration.location.getString(playerName + ".world"));
+				double x = Configuration.location.getDouble(playerName + ".x");
+				double y = Configuration.location.getDouble(playerName + ".y");
+				double z = Configuration.location.getDouble(playerName + ".z");
+				double pit = Configuration.location.getDouble(playerName + ".pitch");
+				double ya = Configuration.location.getDouble(playerName + ".yaw");
+				float pitch = (float) pit;
+				float yaw = (float) ya;
+												
+				Location local = new Location(world, x, y, z, yaw, pitch);
+				player.teleport(local);
+
+				Configuration.location.set(playerName, null);
+				Configuration.location.save();
 				player.setGameMode(GameMode.SURVIVAL);
-				duty.remove(playername);
-				player.setLevel(DoOdyPlayerListener.expOrb.get(playername));
-				expOrb.remove(playername);
-				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playername));
+				duty.remove(playerName);
+				player.setLevel(DoOdyPlayerListener.expOrb.get(playerName));
+				expOrb.remove(playerName);
+				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playerName));
 			} catch (Exception e) {
-				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playername));
+				DoOdyCommandExecutor.myArr.removeAll(Arrays.asList(playerName));
 				player.setGameMode(GameMode.SURVIVAL);
 				player.getInventory().clear();
 			}
@@ -134,20 +209,30 @@ public class DoOdyPlayerListener implements Listener {
 	@EventHandler(ignoreCancelled=true)
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		Player player = event.getEntity();
-		String playername = player.getName();
+		String playerName = player.getName();
 		
-		if(duty.containsKey(playername)) {
+		if(duty.containsKey(playerName)) {
 			event.getDrops().clear();
 			event.setDroppedExp(0);
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		Player player = event.getPlayer();
+		String playerName = player.getName();
+		
+		if(duty.containsKey(playerName) && armour.containsKey(playerName)) {
+			player.getInventory().setArmorContents(armour.get(playerName));
 		}
 	}
 	
 	@EventHandler(priority=EventPriority.HIGH)
 	public void onPlayerDropItem(PlayerDropItemEvent event) {
 		Player player = event.getPlayer();
-		String playername = player.getName();
+		String playerName = player.getName();
 		
-		if (duty.containsKey(playername) && Configuration.config.getBoolean("Duty Deny Drops.enabled", true)) {
+		if (duty.containsKey(playerName) && Configuration.config.getBoolean("Duty Deny Drops.enabled")) {
 			if (!(player.isOp() || player.hasPermission("doody.dropitems"))) {
 				Item item = event.getItemDrop();
 				int itemID = item.getItemStack().getTypeId();
@@ -157,38 +242,71 @@ public class DoOdyPlayerListener implements Listener {
 			
 					event.getItemDrop().remove();
 			
-					if (Configuration.config.getBoolean("Duty Deny Drops.messages", true)) {
+					if (Configuration.config.getBoolean("Duty Deny Drops.messages")) {
 						player.sendMessage(ChatColor.GOLD + "[DoOdy] " + ChatColor.RED + "There's no need to drop " + ChatColor.YELLOW + itemname + ChatColor.RED + " while on Duty.");
+					}
+					if (Configuration.config.getBoolean("Debug.enabled")) {
+						Log.info("[DEBUG] Success! " + playerName + " got denied item drop.");
 					}
 				}
 			} else {
+				if (Configuration.config.getBoolean("Debug.enabled")) {
+					Item item = event.getItemDrop();
+					int itemID = item.getItemStack().getTypeId();
+					String message = item.getItemStack().getType().name();
+					String itemname = message.toLowerCase();
+					if (configDropList.contains(itemID)) {
+						Log.info("[DEBUG] Warning! " + itemname + " is whitelisted in config.");
+						Log.info("[DEBUG] Warning! " + "Allowing " + playerName + " to drop " + itemname);
+					} else {
+						if (player.isOp()) {
+							Log.info("[DEBUG] Warning! " + playerName + " is OP -Allowing item drop, " + itemname);
+						} else if (player.hasPermission("doody.dropitems")) {
+							Log.info("[DEBUG] Warning! " + playerName + " has doody.dropitems -Allowing item drop, " + itemname);
+						} else {
+							//It should not have reached here
+							Log.severe("Another plugin may be causing a conflict. DoOdy Debug cannot make sense. Section onPlayerDropItem in DoOdyPlayerListener");
+						}
+					}
+				}
 				return;
 			}
 		}
 	}
 	
-	@EventHandler(priority=EventPriority.HIGH, ignoreCancelled=true)
+	@EventHandler(priority=EventPriority.HIGHEST, ignoreCancelled=true)
 	public void onPlayerInteract(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
-		String playername = player.getName();
-		int itemID = player.getItemInHand().getTypeId();
-		
-		if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
-			return;			
-		}
-		if (duty.containsKey(playername) && Configuration.config.getBoolean("Deny Storage.enabled", true)) {
-			if (!(player.isOp() || player.hasPermission("doody.storage"))) {
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			Player player = event.getPlayer();
+			String playerName = player.getName();
+			
+			if (duty.containsKey(playerName) && Configuration.config.getBoolean("Deny Storage.enabled")) {
 				Block block = event.getClickedBlock();
 				int blockID = block.getType().getId();
+				
+				if (Configuration.config.getBoolean("Debug.enabled")) {
+					Log.info("[DEBUG] " + playerName + " Right Clicked on " + blockID);
+				}				
 				if (configStorageDenied.contains(blockID)) {
-					if ((configLbTools.contains(itemID) && Configuration.config.getBoolean("Allow.LogBlock.enabled", true)) || (configWeTools.contains(itemID) && Configuration.config.getBoolean("Allow.WorldEdit.enabled", true))) {
-						return;
-					} else {
+					if (!(player.isOp() || player.hasPermission("doody.storage"))) {
 						event.setCancelled(true);
-						if (Configuration.config.getBoolean("Deny Storage.messages", true)) {
-							player.sendMessage(ChatColor.RED + "There's no need to store things while on Duty.");
-							if (Configuration.config.getBoolean("Debug.enabled", true)) {
-								Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "Block ID " + blockID);
+						if (Configuration.config.getBoolean("Deny Storage.messages")) {
+							player.sendMessage(ChatColor.RED + "There's no need to store things while on duty.");
+							if (Configuration.config.getBoolean("Debug.enabled")) {
+								Log.info("[DEBUG] Success! " + playerName + " got denied storage interact.");
+							}
+						}
+					} else {
+						if (Configuration.config.getBoolean("Debug.enabled")) {
+							if (player.isOp()) {
+								Log.info("[DEBUG] Warning! " + playerName + " is OP -Allowing storage interact");
+							} else if (player.hasPermission("doody.storage")) {
+								Log.info("[DEBUG] Warning! " + playerName + " has doody.storage -Allowing storage interact");
+							} else if (!(configStorageDenied.contains(blockID))) {
+								Log.info("[DEBUG] Warning! " + block.getType().name().toLowerCase() + " is not in 'Deny Storage.storage' list -Allowing storage interact");
+							} else {
+								//It should not have reached here
+								Log.severe("Another plugin may be causing a conflict. DoOdy Debug cannot make sense. Section onPlayerInteract in DoOdyPlayerListener");
 							}
 						}
 					}
@@ -199,26 +317,34 @@ public class DoOdyPlayerListener implements Listener {
 	
 	@EventHandler
 	public void onEntityInteract(PlayerInteractEntityEvent event) {
-		Player player = event.getPlayer();
-		String playername = player.getName();
-				
-		if (!(event.getRightClicked() instanceof StorageMinecart)) {
-			return;			
-		} else {		
-			if (duty.containsKey(playername) && (Configuration.config.getBoolean("Deny Storage.enabled", true) && Configuration.config.getBoolean("Deny Storage.Storage Minecart", true))) {
+		if (event.getRightClicked() instanceof StorageMinecart) {
+			Player player = event.getPlayer();
+			String playerName = player.getName();
+			
+			if (duty.containsKey(playerName) && (Configuration.config.getBoolean("Deny Storage.enabled"))) {
 				if (!(player.isOp() || player.hasPermission("doody.storage"))) {
 					event.setCancelled(true);
-					if (Configuration.config.getBoolean("Deny Storage.messages", true)) {
-						player.sendMessage(ChatColor.GOLD + "[DoOdy] " + ChatColor.RED + "There's no need to store things while on Duty.");
-						if (Configuration.config.getBoolean("Debug.enabled", true)) {
-							Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + "Success it was a Storage Minecart");
+					if (Configuration.config.getBoolean("Deny Storage.messages")) {
+						player.sendMessage(ChatColor.RED + "There's no need to store things while on Duty.");
+						if (Configuration.config.getBoolean("Debug.enabled")) {
+							Log.info("[DEBUG] Success! " + playerName + " got denied storage interact.");
+						}
+					}
+				} else {
+					if (Configuration.config.getBoolean("Debug.enabled")) {
+						if (player.isOp()) {
+							Log.info("[DEBUG] Warning! " + playerName + " is OP -Allowing storage interact");
+						} else if (player.hasPermission("doody.storage")) {
+							Log.info("[DEBUG] Warning! " + playerName + " has doody.storage -Allowing storage interact");
+						} else {
+							//It should not have reached here
+							Log.severe("Another plugin may be causing a conflict. DoOdy Debug cannot make sense. Section onEntityInteract in DoOdyPlayerListener");
 						}
 					}
 				}
 			}
 		}
-	}
-	
+	}	
 	
 	/** SLAPI = Saving/Loading API
 	 * API for Saving and Loading Objects.
@@ -237,5 +363,5 @@ public class DoOdyPlayerListener implements Listener {
 			ois.close();
 			return result;
 		}
-	}	
+	}
 }
